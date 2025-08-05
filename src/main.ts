d0aa12bac0c87be7d00ed98a1377f069879df8e7
Yann Cabrel NGATEU TAGNE <ngateuyann8@gmail.com>
2025-08-06 00:22:59 +0200

    Resolve cors problem

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:8080', 'http://127.0.0.1:8080', 'http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   // Swagger configuration
   const config = new DocumentBuilder()
@@ -17,5 +22,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log('API running on http://localhost:3000');
 }
 bootstrap();
